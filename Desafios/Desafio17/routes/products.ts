cd30f0ab5353a93e35427869ba664eb3ff187845
Agustin Rujana <aguru2008@hotmail.com>
2021-03-22 22:33:16 -0300

    commit

---
+++ Desafios/Desafio17/routes/products.ts
@@ -0,0 +1,73 @@
+//Imports
+import { table } from 'node:console'
+import { productoClass } from '../src/constructor'
+
+//Express Initializing
+const express = require('express')
+const router = express.Router()
+
+//Creacion de tabla
+const { options } = require('../db/mysql.db')
+const knex = require('knex')(options)
+
+//Creo la tabla
+// knex.schema.createTable('mensajes', table =>{
+//     table.increments('id')
+//     table.string('email')
+//     table.date('date')
+//     table.string('msg')
+// })
+// .then(() => console.log('Table Created'))
+// .catch((err) => console.log(err))
+// .finally(() => knex.destroy())
+
+//Variables
+let productos: any[] = []
+
+//Services
+const existanceCheck = (id, req, res) => {   
+    const producto: any = productos.find( producto => producto.id === id)
+    if (!producto) {
+        res.sendStatus(404)
+    }
+    return producto
+}
+
+// Products Main
+router.route('/productos')
+    .get((req,res) =>{
+        res.sendFile('form&List.html', {root: './public'})
+    })
+    .post((req, res) =>{
+        const {title, price, thumbnail} = req.body 
+        let productoNuevo = new productoClass(productos.length, title, price, thumbnail)
+        productos.push(productoNuevo)
+        res.sendFile('form&List.html', {root: './public'})
+    })
+
+//Products Lista
+router.route('/productos/vista')
+    .get((req,res) =>{
+        productos.length > 0 ? res.render("productList", {productos, pageTitle : "Lista de productos", listExists: true}) : res.render("productList", {listExists: false})
+    })
+
+// Products :id
+router.route('/productos/:id')
+    .get((req,res: any) =>{
+        existanceCheck(req.params.id, req, res)
+    })
+    .put((req,res) =>{
+        const producto = existanceCheck(req.params.id, req, res)
+        const { title, price, thumbnail } = req.body
+        producto.title = title
+        producto.price = price
+        producto.thumbnail = thumbnail
+    })
+    .delete((req,res) =>{
+        const id: number = req.params.id
+        existanceCheck(id, req, res)
+        productos = productos.filter( producto => producto.id !== id)
+    })
+
+//Exports
+module.exports = router
